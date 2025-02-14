ba70ba60f75569001f1d1dbaa766b071a88975e8
RLeipe <54494365+RLeipe@users.noreply.github.com>
2025-02-14 17:33:00 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>Your Name</title>
+      </Head>
+      <main className="prose prose-invert max-w-none">
+        <h1>Hello.</h1>
+        <p>
+          I build things. Sometimes they work.
+        </p>
+      </main>
+    </Layout>
+  )
+}
